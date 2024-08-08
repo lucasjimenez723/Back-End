ceb82df69b8f2765d6cf70cf59eae34666154139
lucasjimenez723 <lucasjimenez553@gmail.com>
2024-08-08 12:35:22 -0300

    cambios final

--- Proyecto final/routes/tickets.router.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express').Router;
-const TicketController = require('../controller/ticket.controller');
-
-const ticketRouter = Router();
-
-ticketRouter.get('/', TicketController.getAllTickets);
-
-
-module.exports = ticketRouter;
