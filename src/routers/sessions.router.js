bf1ca8ce67a7f98cf5b0013bb1b89644ae3e8127
lucasjimenez723 <lucasjimenez553@gmail.com>
2024-08-10 15:55:08 -0300

    cambio final

--- src/routers/sessions.router.js
+++ src/routers/sessions.router.js
@@ -19,9 +19,9 @@ import userDTO from "../dao/DTOs/userDTO.js";
 
 const router = Router()
 router.use(addLogger);
-router.post('/register' , passport.authenticate('register',{session:false, failureRedirect:'/failregister'}), register);
+router.post('/register' , passport.authenticate('register',{session:false, failureRedirect:'failregister'}), register);
 router.get('/failregister', failRegister);
-router.post('/login', passport.authenticate('login',{session: false, failureRedirect:'/faillogin'}), loginJWT);
+router.post('/login', passport.authenticate('login',{session: false, failureRedirect:'faillogin'}), loginJWT);
 router.get('/faillogin', failLogin);
 router.get('/current2', async (req, res) => {
     res.send(req.user); 
@@ -33,7 +33,7 @@ router.get('/current', passportCall('jwt'), applyPolicy(['USER' , 'PREMIUM']), (
 })
 router.get('/github' , passport.authenticate('github',{scope:['user:email']}), async(req,res) =>{
 })
-router.get('/githubcallback', passport.authenticate('github',{session:false, failureRedirect:'/login'}), gitHubCallBack)
+router.get('/githubcallback', passport.authenticate('github',{session:false, failureRedirect:'login'}), gitHubCallBack)
 router.get('/logout',passportCall('jwt'), logOutJwt);
 router.post('/resetpassword', resetPassword);
 router.put('/newpassword', newPassword);
